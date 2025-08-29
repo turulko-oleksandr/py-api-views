d31888d059d0cc5672a7c142c1ab9142d22b8bb9
turulko-oleksandr <sashaturulko@i.ua>
2025-08-29 16:16:36 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -20,8 +20,8 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
-    actors = models.ManyToManyField(Actor, related_name='movies')
-    genres = models.ManyToManyField(Genre, related_name='movies')
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self):
         return self.title
